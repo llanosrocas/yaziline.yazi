5886330df6fdb5f7f8658276dccce9a585e83404
llanosrocas <llanosrocas@gmail.com>
2024-07-28 21:07:28 +0300

    chore: remove unused code

--- init.lua
+++ init.lua
@@ -2,10 +2,10 @@ local function setup(_, options)
   options = options or {}
 
   local default_separators = {
-    angly = {"", "", "", ""},
-    curvy = {"", "", "", ""},
-    liney = {"", "", "|", "|"},
-    empty = {"", "", "", ""}
+    angly = { "", "", "", "" },
+    curvy = { "", "", "", "" },
+    liney = { "", "", "|", "|" },
+    empty = { "", "", "", "" }
   }
   local separators = default_separators[options.separator_style or "angly"]
 
@@ -35,9 +35,7 @@ local function setup(_, options)
     end
 
     local style = self:style()
-    return ui.Line {
-      ui.Span(" " .. mode .. " "):style(style),
-    }
+    return ui.Span(" " .. mode .. " "):style(style)
   end
 
   function Status:size()
@@ -47,9 +45,8 @@ local function setup(_, options)
     end
 
     local style = self:style()
-    return ui.Line {
-      ui.Span(current_separator_style.separator_close .. " " .. ya.readable_size(h:size() or h.cha.length) .. " "):fg(style.bg):bg(THEME.status.separator_style.bg),
-    }
+    return ui.Span(current_separator_style.separator_close .. " " .. ya.readable_size(h:size() or h.cha.length) .. " ")
+        :fg(style.bg):bg(THEME.status.separator_style.bg)
   end
 
   function Status:name()
@@ -58,7 +55,9 @@ local function setup(_, options)
       return ui.Line {}
     end
 
-    local trimmed_name = #h.name > config.filename_max_length and (string.sub(h.name, 1, config.filename_trim_length) .. "..." .. string.sub(h.name, -config.filename_trim_length)) or h.name
+    local trimmed_name = #h.name > config.filename_max_length and
+        (string.sub(h.name, 1, config.filename_trim_length) .. "..." .. string.sub(h.name, -config.filename_trim_length)) or
+        h.name
 
     local style = self:style()
     return ui.Line {
@@ -74,8 +73,8 @@ local function setup(_, options)
 
     local selected_fg = files_selected > 0 and THEME.manager.count_selected.bg or THEME.status.separator_style.fg
     local yanked_fg = files_yanked > 0 and
-    (files_is_cut and THEME.manager.count_cut.bg or THEME.manager.count_copied.bg) or
-    THEME.status.separator_style.fg
+        (files_is_cut and THEME.manager.count_cut.bg or THEME.manager.count_copied.bg) or
+        THEME.status.separator_style.fg
 
     local yanked_text = files_yanked > 0 and config.yank_symbol .. " " .. files_yanked or config.yank_symbol .. " 0"
 
@@ -91,38 +90,8 @@ local function setup(_, options)
     local cha = hovered.cha
     local time = (cha.modified or 0) // 1
 
-    return ui.Line {
-      ui.Span(os.date("%Y-%m-%d %H:%M", time) .. " " .. current_separator_style.separator_open_thin .. " "):fg(THEME.status.separator_style.fg),
-    }
-  end
-
-  function Status:permissions()
-    local h = self._tab.current.hovered
-    if not h then
-      return ui.Line {}
-    end
-
-    local perm = h.cha:permissions()
-    if not perm then
-      return ui.Line {}
-    end
-
-    local spans = {}
-    for i = 1, #perm do
-      local c = perm:sub(i, i)
-      local style = THEME.status.permissions_t
-      if c == "-" or c == "?" then
-        style = THEME.status.permissions_s
-      elseif c == "r" then
-        style = THEME.status.permissions_r
-      elseif c == "w" then
-        style = THEME.status.permissions_w
-      elseif c == "x" or c == "s" or c == "S" or c == "t" or c == "T" then
-        style = THEME.status.permissions_x
-      end
-      spans[i] = ui.Span(c):style(style)
-    end
-    return ui.Line(spans)
+    return ui.Span(os.date("%Y-%m-%d %H:%M", time) .. " " .. current_separator_style.separator_open_thin .. " "):fg(
+      THEME.status.separator_style.fg)
   end
 
   function Status:percentage()
@@ -154,23 +123,11 @@ local function setup(_, options)
     local length = #self._tab.current.files
 
     local style = self:style()
-    return ui.Line {
-      ui.Span(string.format(" %2d/%-2d ", cursor + 1, length)):style(style),
-    }
+    return ui.Span(string.format(" %2d/%-2d ", cursor + 1, length)):style(style)
   end
 
-  Status._left = {
-    { Status.mode, id = 1, order = 1000 },
-    { Status.size, id = 2, order = 2000 },
-    { Status.name, id = 3, order = 3000 },
-    { Status.files, id = 4, order = 4000 },
-  }
-    Status._right = {
-    { Status.modified, id = 5, order = 5000 },
-    { Status.permissions, id = 6, order = 6000 },
-    { Status.percentage, id = 7, order = 7000 },
-    { Status.position, id = 8, order = 8000 },
-  }
+  Status:children_add(Status.files, 4000, Status.LEFT)
+  Status:children_add(Status.modified, 0, Status.RIGHT)
 end
 
 return { setup = setup }
